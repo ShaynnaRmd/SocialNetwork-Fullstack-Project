0b9cd286941792968ff71014314c5431c66c59d3
GDKobrae <djedjegboble.pro@gmail.com>
2023-05-12 09:21:19 +0200

    Adding Register page control change

--- classlink_app/connections/login.php
+++ classlink_app/connections/login.php
@@ -1,7 +1,16 @@
 <?php
     session_start();
-    $method = filter_input(INPUT_SERVER, "METHOD");
-    
+    $method = filter_input(INPUT_SERVER, "REQUEST_METHOD");
+    if ($method =="POST") {
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        if ($username && $password) {
+            $data = array(
+                "login" => $username,
+                "password" => $password
+            );
+        }
+    }
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,10 +25,10 @@
         <h2>Se connecter</h2>
         <form action="" method="POST">
             <label for="username">Identifiant: </label>
-            <input type="text" id="username" name="username" placeholder="Identifiant">
+            <input type="text" id="username" name="username" placeholder="Identifiant" required>
 
             <label for="password">Mot de passe: </label>
-            <input type="password" id="password" name="password" placeholder="Mot de passe">
+            <input type="password" id="password" name="password" placeholder="Mot de passe" required>
 
             <input type="submit" value="Connexion">
         </form>
