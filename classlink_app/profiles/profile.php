47600d0ba08895b403ba12176df8e9245b37ef6a
william <williams_wandji@yahoo.fr>
2023-05-15 10:13:52 +0200

    aze

--- classlink_app/profiles/profile.php
+++ classlink_app/profiles/profile.php
@@ -5,10 +5,28 @@
     $data = json_decode($json,true);
     
     $username = $data["username"];
-    $password = $data["password"];
+    $birth_date = $data ['birth_date'];
     $first_name = $data["first_name"];
     $last_name = $data['last_name'];
     $mail = $data["mail"];
     $gender = $data["gender"];
+
+    $requete_recuperation_profile = $auth_pdo->prepare("
+    INSERT INTO profiles (username,birth_date,first_name,last_name,mail,gender)
+    VALUES (:username, :birth_date, :first_name, :last_name, :mail, :gender)
+    ");
+
+    $requete_recuperation_profile->execute([
+      ":username"=> $username,
+      "birth_date"=> $birth_date,
+      ":first_name"=> $first_name,
+      ":last_name"=> $last_name,
+      ":mail"=> $mail,
+      ":gender"=> $gender
+    ]);
+    
+
+
+
 
 
