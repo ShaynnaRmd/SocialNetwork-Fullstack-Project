b3b80388d5c28401d8d65eb43dee90483ab713c3
william <williams_wandji@yahoo.fr>
2023-05-17 11:23:43 +0200

    end create pages

--- classlink_app/pages/create_page.php
+++ classlink_app/pages/create_page.php
@@ -4,22 +4,25 @@ session_start();
 
 $title = "Créer une page";
 
+
+
 if(!isset($_SESSION["token"]) &&  !isset($_SESSION['id'])){
     header('Location: ../connections/login.php');
     exit();
 }
+echo $_SESSION['id'];
 
-$methode = filter_input(INPUT_SERVER, 'REQUEST_METHOD');
+$method = filter_input(INPUT_SERVER, 'REQUEST_METHOD');
 
-if($methode == 'POST'){
-    $name_page = filter_input(INPUT_POST,'name_page');
-    $description = filter_input(INPUT_POST,'description');
-    $pp_image = filter_input(INPUT_POST,'pp_image');
-    $banner_image = filter_input(INPUT_POST,'banner_image');
+if($method == 'POST'){
+    $name_page = filter_input(INPUT_POST,"name_page");
+    $description = filter_input(INPUT_POST,"description");
+    $pp_image = filter_input(INPUT_POST,"pp_image");
+    $banner_image = filter_input(INPUT_POST,"banner_image");
 
-    $verify_existing_page_request = $ticket_pdo->prepare("
+    $verify_existing_page_request = $app_pdo->prepare("
         SELECT * FROM pages 
-        WHERE name_page = :name_page;
+        WHERE name = :name_page;
     ");
     $verify_existing_page_request->execute([
         ":name_page" => $name_page
@@ -27,28 +30,33 @@ if($methode == 'POST'){
 
     $verify_existing_pages = $verify_existing_page_request ->fetch(PDO::FETCH_ASSOC);
 
-    if(!$verify_existing_page_request){
-        $create_page_request1 = $app_pdo ->prepare("
-        SELECT profiles.id FROM profiles
-        JOIN pages ON profiles.id = pages.creator_profile_id
-        ");
-        // $create_page_request1->execute()
-
+    if(!$verify_existing_pages){
+        if(isset($_SESSION['id'])){
+            $request_pages_creator_profile_id = $app_pdo -> prepare('
+            SELECT * FROM profiles WHERE id = :id
+            ');
+            $request_pages_creator_profile_id->execute([
+            ':id' => $_SESSION['id']
+            ]);
+            }
+       
+        $creator_profile_id = $_SESSION['id'];
         $create_page_request = $app_pdo -> prepare('
-        INSERT INTO pages (name_page,description, pp_image, banner_image)
-        VALUES (:name_page, :description, :pp_image, :banner_image);
+        INSERT INTO pages (name,description, pp_image, banner_image,creator_profile_id)
+        VALUES (:name_page, :description, :pp_image, :banner_image,:creator_profile_id);
         ');
         $create_page_request->execute([
+            ':creator_profile_id' => $creator_profile_id,
             ':name_page' => $name_page,
             ':description' => $description,
             ':pp_image' => $pp_image,
             ':banner_image' => $banner_image
         ]);
-        echo 'bien créer';
-    }
-    else{
-        echo 'Ce nom de page existe déjà';
-    }
+        echo 'Bien créer';
+        }
+        else{
+            echo 'Ce nom de page existe déjà';
+        }
 }
 ?>
 
@@ -62,7 +70,7 @@ if($methode == 'POST'){
 </head>
 <body>
     <h1><?= $title ?></h1>
-    <form methode = "POST">
+    <form method = "POST">
         <label for = "name_page"></label>
         <input type="text" name = "name_page" placeholder = "Nom de la page">
         <label for = "description"></label>
